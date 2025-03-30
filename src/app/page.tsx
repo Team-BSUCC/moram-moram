d4282b7c4c90c22b887c1f6ea1c316e89eb27692
KGyeong <rlarudals61@naver.com>
2025-03-30 18:17:41 +0900

    feature:#1 인증 설정 초기 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-const HomePage = () => {
-  return <div>page</div>;
-};
+import { getServerClient } from '@/shared/utils/supabase/server-client';
 
-export default HomePage;
+export default async function HomePage() {
+  const supabase = getServerClient();
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (error || !user) {
+    return <div>로그인이 필요합니다.</div>;
+  }
+
+  return <div>로그인된 유저의 이메일: {user.email}</div>;
+}
---
+++ src/shared/utils/supabase/server-client.ts
@@ -0,0 +1,22 @@
+import { cookies } from 'next/headers';
+import { createServerClient } from '@supabase/ssr';
+
+export const getServerClient = () => {
+  const cookieStore = cookies();
+  return createServerClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_API_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            cookieStore.set(name, value, options)
+          );
+        },
+      },
+    }
+  );
+};
