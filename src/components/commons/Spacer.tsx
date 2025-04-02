1247ca5605fda161c2ff0688b8447bec9712baf9
taejin7088 <taegin70888@gmail.com>
2025-04-02 15:23:20 +0900

    feature:#11 commons/Spacer 컴포넌트 생성 및 기능구현

---
+++ src/components/commons/Spacer.tsx
@@ -0,0 +1,40 @@
+import { cva, VariantProps } from 'class-variance-authority';
+import { twMerge } from 'tailwind-merge';
+
+export const spacerVariants = cva(
+  `
+    w-full
+  `,
+  {
+    variants: {
+      size: {
+        //예시로 넣어둠
+        xs: 'h-1', // 4px
+        sm: 'h-2', // 8px
+        md: 'h-4', // 16px
+        lg: 'h-8', // 32px
+        xl: 'h-12', // 48px
+        '2xl': 'h-16', // 64px
+        '3xl': 'h-20', // 80px
+      },
+    },
+    defaultVariants: {
+      size: 'md',
+    },
+  }
+);
+
+type SpacerProps = VariantProps<typeof spacerVariants>;
+
+/**
+ * Spacer 공통 컴포넌트 - 요소들 사이에 위아래 간격을 제공합니다
+ * @param size - 간격 크기 (xs, sm, md, lg, xl, 2xl, 3xl)
+ * @returns - 간격을 제공하는 div 요소
+ */
+const Spacer = ({ size }: SpacerProps) => {
+  return (
+    <div className={twMerge(spacerVariants({ size }))} aria-hidden='true' />
+  );
+};
+
+export default Spacer;
