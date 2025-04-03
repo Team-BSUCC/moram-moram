7b0fa311d1ead5a046619ac7ca4ead57a2ab903c
taejin7088 <taegin70888@gmail.com>
2025-04-03 11:30:15 +0900

    feature:#12 layout header,footer영역 구분

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='ko-kr'>
-      <body className='antialiased'>{children}</body>
+    <html lang='ko-kr' className='h-full w-full'>
+      <body className='flex h-full w-full flex-col antialiased'>
+        <header className='fixed left-0 right-0 top-0 z-50 h-[60px]'>
+          헤더
+          {/* <Header /> 컴포넌트가 들어올 예정입니다. */}
+        </header>
+        <main className='mt-[60px] flex-grow'>{children}</main>
+        <footer>
+          푸터
+          {/* <Footer /> 컴포넌트가 들어올 예정입니다. */}
+        </footer>
+      </body>
     </html>
   );
 }
