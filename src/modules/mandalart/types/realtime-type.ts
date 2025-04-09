6e2f1acb2103ecf23455b414bd2856a7e17d11f6
taejin7088 <taegin70888@gmail.com>
2025-04-09 14:13:59 +0900

    refactor#45 payload타입 확장성을 위해 분리

--- src/modules/mandalart/types/realtime-type.ts
+++ src/modules/mandalart/types/realtime-type.ts
@@ -1,12 +1,24 @@
 import { Tables } from '@/shared/types/database.types';
 
+export type TopicPayloadType = Tables<'mandalart_topics'> & {
+  category: 'TOPIC';
+  value: string;
+};
+
+export type SubtopicPayloadType = Tables<'mandalart_subtopics'> & {
+  category: 'SUBTOPIC';
+  value: string;
+};
+
+export type TodoPayloadType = Tables<'cell_todos'> & {
+  action: string;
+  value: string;
+  category: 'TODO';
+};
+
 export type BroadcastPayloadType =
-  | (Tables<'mandalart_topics'> & { category: 'TOPIC'; value: string })
-  | (Tables<'mandalart_subtopics'> & { category: 'SUBTOPIC'; value: string })
-  | (Tables<'cell_todos'> & {
-      action: string;
-      value: string;
-      category: 'TODO';
-    });
+  | TopicPayloadType
+  | SubtopicPayloadType
+  | TodoPayloadType;
 
 export type PartialBroadcastPayloadType = Partial<BroadcastPayloadType>;
