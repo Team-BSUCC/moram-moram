29567a87fd17e0bdb065162653cdd35aff9d3af4
taejin7088 <taegin70888@gmail.com>
2025-04-09 14:44:55 +0900

    refactor#50 브로드캐스트 스토어 타입 수정

--- src/modules/mandalart/hooks/use-realtime-broadcast-batch.ts
+++ src/modules/mandalart/hooks/use-realtime-broadcast-batch.ts
@@ -1,24 +1,16 @@
-import { Tables } from '@/shared/types/database.types';
 import { useRef } from 'react';
+import {
+  BroadcastPayloadType,
+  SubtopicPayloadType,
+  TodoPayloadType,
+  TopicPayloadType,
+} from '../types/realtime-type';
 
 type BroadcastStoreType = {
-  topic: Map<string, Tables<'mandalart_topics'>>;
-  subTopic: Map<string, Tables<'mandalart_subtopics'>>;
-  todo: Map<
-    string,
-    Tables<'cell_todos'> & {
-      action: string;
-    }
-  >;
+  topic: Map<string, TopicPayloadType>;
+  subTopic: Map<string, SubtopicPayloadType>;
+  todo: Map<string, TodoPayloadType>;
 };
-
-type PayloadType =
-  | (Tables<'mandalart_topics'> & { category: 'TOPIC' })
-  | (Tables<'mandalart_subtopics'> & { category: 'SUBTOPIC' })
-  | (Tables<'cell_todos'> & {
-      action: string;
-      category: 'TODO';
-    });
 
 export const useRealtimeBroadcastBatch = () => {
   const broadcastStore = useRef<BroadcastStoreType>({
@@ -28,22 +20,23 @@ export const useRealtimeBroadcastBatch = () => {
   });
 
   //나중에 페이로드 객체 정의되면 페이로드 객체 인자로 받을 예정
-  const addBroadcastStore = (payload: PayloadType) => {
+  const addBroadcastStore = (payload: BroadcastPayloadType) => {
     //payload에 topic이 들어오면 실행
-    if ('topic_index' in payload) {
-      broadcastStore.current.topic.set(key, value);
+    if (payload.category === 'TOPIC') {
+      broadcastStore.current.topic.set(payload.id, payload);
     }
     //payload에 subtopic이 들어오면
-    if ('cell_index' in payload) {
-      broadcastStore.current.subTopic.set(key, value);
+    if (payload.category === 'SUBTOPIC') {
+      broadcastStore.current.subTopic.set(payload.id, payload);
     }
     //payload에 todo가 들어오면
-    if ('action' in payload) {
-      broadcastStore.current.todo.set(key, value);
+    if (payload.category === 'TODO') {
+      broadcastStore.current.todo.set(payload.id, payload);
     }
   };
 
   const batchUpdateSupabase = async () => {
+    //수파베이스 업데이트 일괄 업데이트 로직
     broadcastStore.current.topic.clear();
     broadcastStore.current.subTopic.clear();
     broadcastStore.current.todo.clear();
