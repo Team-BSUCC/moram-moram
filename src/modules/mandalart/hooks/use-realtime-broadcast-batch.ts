9854eb77eabd7fa9f936b4d3470c42082d0419bb
taejin7088 <taegin70888@gmail.com>
2025-04-10 01:09:27 +0900

    feature#50 전달받은 브로드캐스트 스토어로 해당쿼리키 state 변경

--- src/modules/mandalart/hooks/use-realtime-broadcast-batch.ts
+++ src/modules/mandalart/hooks/use-realtime-broadcast-batch.ts
@@ -6,11 +6,24 @@ import {
   TopicPayloadType,
 } from '../types/realtime-type';
 import { mandalartBatchUpdateSupabase } from '../services/mandalart-batch-update-supabase-service';
+import { useQueryClient } from '@tanstack/react-query';
 
 export type BroadcastStoreType = {
   topic: Map<string, TopicPayloadType>;
   subTopic: Map<string, SubtopicPayloadType>;
   todo: Map<string, TodoPayloadType>;
+};
+
+type FormatBroadcastStorePayloadType = {
+  topic: {
+    [k: string]: TopicPayloadType;
+  };
+  subTopic: {
+    [k: string]: SubtopicPayloadType;
+  };
+  todo: {
+    [k: string]: TodoPayloadType;
+  };
 };
 
 export const useRealtimeBroadcastBatch = () => {
@@ -36,6 +49,7 @@ export const useRealtimeBroadcastBatch = () => {
     }
   };
 
+  //브로드캐스트로 주고받던 변경된 데이터들을 수파베이스에 업로드
   const batchUpdateSupabase = async () => {
     try {
       await mandalartBatchUpdateSupabase(broadcastStore);
@@ -48,11 +62,64 @@ export const useRealtimeBroadcastBatch = () => {
     }
   };
 
+  //페이로드로 보낼 브로드캐스트를 리턴
+  const formatBroadcastStorePayload = () => {
+    return {
+      topic: Object.fromEntries(broadcastStore.current.topic),
+      subTopic: Object.fromEntries(broadcastStore.current.subTopic),
+      todo: Object.fromEntries(broadcastStore.current.todo),
+    };
+  };
+
+  //페이로드로 받은 브로드캐스트로 현재 브로드캐스트 업데이트
+  const queryClient = useQueryClient();
+  const receiveBroadcastStore = (
+    payloadBroadcastStore: FormatBroadcastStorePayloadType
+  ) => {
+    broadcastStore.current = {
+      topic: new Map(Object.entries(payloadBroadcastStore.topic || {})),
+      subTopic: new Map(Object.entries(payloadBroadcastStore.subTopic || {})),
+      todo: new Map(Object.entries(payloadBroadcastStore.todo || {})),
+    };
+
+    //topic 관련 텐스텍 state 변경
+    if (broadcastStore.current.topic.size !== 0) {
+      broadcastStore.current.topic.forEach((topicPayload, topicId) => {
+        queryClient.setQueryData(['topic', topicId], topicPayload.value);
+      });
+    }
+
+    //subtopic 관련 텐스텍 state 변경
+    if (broadcastStore.current.subTopic.size !== 0) {
+      broadcastStore.current.topic.forEach((subtopicPayload, subtopicId) => {
+        queryClient.setQueryData(
+          ['subtopic', subtopicId],
+          subtopicPayload.value
+        );
+      });
+    }
+
+    //todo 관련 텐스텍 state 변경
+    if (broadcastStore.current.todo.size !== 0) {
+      broadcastStore.current.todo.forEach((todoPayload, todoId) => {
+        if (todoPayload.action !== 'DELETE') {
+          //소주제를 구분할 수 있는 쿼리키가 추가되어야 할 듯
+          queryClient.setQueryData(['todo', todoId], todoPayload.value);
+        }
+      });
+    }
+  };
+
   useEffect(() => {
     return () => {
       batchUpdateSupabase();
     };
   }, []);
 
-  return { addBroadcastStore, batchUpdateSupabase };
+  return {
+    addBroadcastStore,
+    batchUpdateSupabase,
+    formatBroadcastStorePayload,
+    receiveBroadcastStore,
+  };
 };
