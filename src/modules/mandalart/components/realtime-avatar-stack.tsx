3741eefa13a8cb310c71a77d89bcf34030f8d3c2
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-04-11 10:33:30 +0900

    move:#34 파일, 폴더 정리

---
+++ src/modules/mandalart/components/realtime-avatar-stack.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { AvatarStack } from '@/modules/mandalart/components/avatar-stack';
+
+import { useMemo } from 'react';
+import { useRealtimePresenceRoom } from '../hooks/use-realtime-presence-room';
+
+export const RealtimeAvatarStack = ({ roomName }: { roomName: string }) => {
+  const { users: usersMap } = useRealtimePresenceRoom(roomName);
+  const avatars = useMemo(() => {
+    return Object.values(usersMap).map((user) => ({
+      name: user.name,
+      image: user.image,
+    }));
+  }, [usersMap]);
+
+  return <AvatarStack avatars={avatars} />;
+};
