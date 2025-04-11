51d9bbc5efae6caa545f6e4f986018cf3ec09a5e
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-04-11 15:08:32 +0900

    refactor:#48 useQuery 커스텀 훅 분리

--- src/modules/mandalart/components/subtopic-group.tsx
+++ src/modules/mandalart/components/subtopic-group.tsx
@@ -1,14 +1,9 @@
-import { QUERY_KEY } from '@/shared/constants/query-key';
 import TodoItem from './todo-item';
-import { useQuery } from '@tanstack/react-query';
 import { SubTopicType, TodoType } from '../types/realtime-type';
+import { useSubtopicCacheQuery } from '../hooks/use-mandalart-data-query';
 
 const SubtopicGroup = ({ sub }: { sub: SubTopicType }) => {
-  const { data: subtopicName } = useQuery({
-    queryKey: QUERY_KEY.subtopic(sub.id),
-    queryFn: () => Promise.resolve(null),
-    enabled: false,
-  });
+  const { data: subtopicName } = useSubtopicCacheQuery(sub.id);
   return (
     <div key={sub.id} className='pl-4'>
       <div>{subtopicName}</div>
--- src/modules/mandalart/components/topic-group.tsx
+++ src/modules/mandalart/components/topic-group.tsx
@@ -1,15 +1,10 @@
 import Text from '@/components/commons/text';
-import { QUERY_KEY } from '@/shared/constants/query-key';
 import { TopicType } from '../types/realtime-type';
-import { useQuery } from '@tanstack/react-query';
 import SubtopicGroup from './subtopic-group';
+import { useTopicCacheQuery } from '../hooks/use-mandalart-data-query';
 
 const TopicGroup = ({ topic }: { topic: TopicType }) => {
-  const { data: topicName } = useQuery({
-    queryKey: QUERY_KEY.topic(topic.id),
-    queryFn: () => Promise.resolve(null),
-    enabled: false,
-  });
+  const { data: topicName } = useTopicCacheQuery(topic.id);
   return (
     <div className='pl-2'>
       <div className='text-blue-700'>{topicName}</div>
