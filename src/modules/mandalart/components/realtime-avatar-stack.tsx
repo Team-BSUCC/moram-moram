36ad447f3b018957769643fbab6e7b93943ca598
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-04-11 10:44:59 +0900

    chore:#34 타입 표기 방법 수정

--- src/modules/mandalart/components/realtime-avatar-stack.tsx
+++ src/modules/mandalart/components/realtime-avatar-stack.tsx
@@ -5,7 +5,11 @@ import { AvatarStack } from '@/modules/mandalart/components/avatar-stack';
 import { useMemo } from 'react';
 import { useRealtimePresenceRoom } from '../hooks/use-realtime-presence-room';
 
-export const RealtimeAvatarStack = ({ roomName }: { roomName: string }) => {
+type Props = {
+  roomName: string;
+};
+
+export const RealtimeAvatarStack = ({ roomName }: Props) => {
   const { users: usersMap } = useRealtimePresenceRoom(roomName);
   const avatars = useMemo(() => {
     return Object.values(usersMap).map((user) => ({
