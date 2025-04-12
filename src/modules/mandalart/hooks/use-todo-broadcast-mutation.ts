b134142a4d2a15498c029916a7eaaf663d7a239e
KGyeong <rlarudals61@naver.com>
2025-04-13 04:47:45 +0900

    feature:#65 Todo 객체의 action을 파악하여 그에 따라 payload에 넣어 broadcast하는 mutation 함수 제작

---
+++ src/modules/mandalart/hooks/use-todo-broadcast-mutation.ts
@@ -0,0 +1,36 @@
+import { RealtimeChannel } from '@supabase/supabase-js';
+import { TodoPayloadType } from '../types/realtime-type';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { QUERY_KEY } from '@/shared/constants/query-key';
+import { judgingAction } from '../services/judging-action';
+
+export const useTodoBroadcastMutation = (
+  myChannel: RealtimeChannel,
+  todoRowData: TodoPayloadType
+) => {
+  const queryClient = useQueryClient();
+
+  const todoListKey: readonly unknown[] = QUERY_KEY.todolist(
+    todoRowData.cell_id
+  );
+
+  const mutationUpdateCache = useMutation({
+    onMutate: async () => judgingAction(queryClient, todoListKey, todoRowData),
+    mutationFn: async () => {
+      if (!myChannel) throw new Error('채널없음');
+      await myChannel.send({
+        type: 'broadcast',
+        event: 'shout',
+        payload: todoRowData,
+      });
+    },
+    onError: () => {
+      /**
+       * TODO: error 핸들링 sentry 리팩터링
+       */
+      console.error('broadcast에 오류가 발생했습니다.');
+    },
+  });
+
+  return { ...mutationUpdateCache };
+};
