e5ce219eb4597cd08d939a4ed144d66028bc64d1
KGyeong <rlarudals61@naver.com>
2025-04-15 14:57:03 +0900

    feature:#58 guest page 제작

---
+++ src/app/(non-auth)/guest/page.tsx
@@ -0,0 +1,83 @@
+'use client';
+import Button from '@/components/commons/button';
+import Spacer from '@/components/commons/spacer';
+import Text from '@/components/commons/text';
+import Title from '@/components/commons/title';
+import GuestMandalartBlock from '@/modules/guest/components/guest-mandalart-block';
+import GuestMandalartMainBlock from '@/modules/guest/components/guest-mandalart-main-block';
+import { useGuestTopicStore } from '@/modules/guest/hooks/use-guest-topic-store';
+import { toPng } from 'html-to-image';
+import React, { useRef } from 'react';
+
+const GuestPage = () => {
+  const topicColorList = [
+    'bg-pink-pastel',
+    'bg-red-pastel',
+    'bg-orange-pastel',
+    'bg-purple-pastel',
+    'bg-yellow-pastel',
+    'bg-blue-pastel',
+    'bg-sky-pastel',
+    'bg-green-pastel',
+    'bg-transparent',
+  ];
+
+  const title = useGuestTopicStore((state) => state.core);
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleDownload = async () => {
+    if (ref.current === null) return;
+
+    try {
+      const dataUrl = await toPng(ref.current);
+      const link = document.createElement('a');
+      link.download = 'exported-image.png';
+      link.href = dataUrl;
+      link.click();
+    } catch (error) {
+      console.error('이미지 변환 실패:', error);
+    }
+  };
+
+  return (
+    <div className='flex-col'>
+      <Spacer size='3xl' />
+      <div className='bg-white-light p-2 md:p-5' ref={ref}>
+        <div className='w-full flex-col justify-items-center'>
+          <Title variant='default' as='h1' highlightColor='bg-red-500'>
+            {title || '나만의 만다라트 작성하기'}
+          </Title>
+          <Spacer size='lg' />
+          <Text variant='default' size='default' align='center'>
+            지금 당신의 목표를 81칸에 정리해보세요. 작은 계획들이 큰 변화를
+            만듭니다.
+          </Text>
+          <Spacer size='xl' />
+        </div>
+        <div className='grid w-fit grid-cols-3 grid-rows-3 gap-2 text-ss md:gap-5 md:text-md'>
+          <div className='col-start-2 row-start-2 aspect-square h-full'>
+            <GuestMandalartMainBlock
+              coreColor='bg-violet-pigment'
+              topicColor={topicColorList}
+            />
+          </div>
+          {Array.from({ length: 8 }).map((_, index) => (
+            <div key={`block${index}`}>
+              <GuestMandalartBlock index={index} topicColor={topicColorList} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <Spacer size='3xl' />
+      <div className='flex w-full justify-center'>
+        <Button variant='outline' size='default' onClick={handleDownload}>
+          이미지 저장하기
+        </Button>
+      </div>
+      <Spacer size='3xl' />
+    </div>
+  );
+};
+
+export default GuestPage;
