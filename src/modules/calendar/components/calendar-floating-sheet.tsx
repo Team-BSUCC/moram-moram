229b3356eb1f6e105e3634654d68f7cf95b68e14
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-04-15 22:19:02 +0900

    refactor:#83 데이터 타입 추가

--- src/modules/calendar/components/calendar-floating-sheet.tsx
+++ src/modules/calendar/components/calendar-floating-sheet.tsx
@@ -2,32 +2,38 @@ import FloatingSheet from '@/components/commons/floating-sheet';
 import Text from '@/components/commons/text';
 import Title from '@/components/commons/title';
 import useFloatingSheetStore from '@/shared/hooks/use-floating-sheet-store';
+import { CoreType, EventType } from '../type/todo-type';
 
-type CalendarProps = {
-  todos: any;
-  events: any;
+type CalendarFloatingSheetProps = {
+  todos: CoreType[] | undefined;
+  events: EventType[] | undefined;
 };
 
-const CalendarFloatingSheet = ({ todos: data, events }: CalendarProps) => {
+const CalendarFloatingSheet = ({
+  todos: data,
+  events,
+}: CalendarFloatingSheetProps) => {
+  // 달력에서 선택한 날짜
   const info = useFloatingSheetStore((state) => state.info as string);
+  // 클릭한 날짜에 해당하는 투두 목록을 가져오기 위한 필터링
   const isSatisfied =
-    events.filter((event) => event.date.slice(0, 10) === info).length > 0;
+    events &&
+    events?.filter((event) => event.date.slice(0, 10) === info).length > 0;
 
   return (
     <FloatingSheet>
       <Text>{info}</Text>
       <Title as='h1'>TO DO LIST</Title>
-
-      {isSatisfied ? (
+      {isSatisfied && data ? (
         data.map((core) => (
           <div key={core.title}>
-            <Title as='h2'>{core.title}</Title> {/* 핵심주제 */}
-            {core.topics.map((topic) =>
-              topic.subtopics.map((sub) =>
+            <Title as='h2'>{core.title}</Title>
+            {core.topics.map((topic, topicIdx) =>
+              topic.subtopics.map((sub, subIdx) =>
                 sub.todos
                   .filter((todo) => todo.createdAt.slice(0, 10) === info)
                   .map((todo, idx) => (
-                    <div key={idx}>
+                    <div key={`${topicIdx}-${subIdx}-${idx}`}>
                       <div>{todo.title}</div>
                       <div className='text-gray-500 text-sm'>
                         {topic.title} &gt; {sub.title}
--- src/modules/calendar/services/get-fetch-calendar-data.ts
+++ src/modules/calendar/services/get-fetch-calendar-data.ts
@@ -1,8 +1,21 @@
 import { getBrowserClient } from '@/shared/utils/supabase/browser-client';
+import {
+  CoreTempType,
+  ProcessedDataType,
+  RoomFetchType,
+  SubtopicTempType,
+  TodoTempType,
+  TopicTempType,
+} from '../type/fetch-calendar-type';
 
+/**
+ * @todo 투두 모아보기 페이지에서도 사용가능하게 수정
+ * 캘린더 페이지에 사용될 투두 목록을 받아오는 함수
+ * @returns 가공된 데이터
+ */
 export const getFetchCalendarData = async () => {
   const supabase = getBrowserClient();
-  const { data, error }: { data: any; error: any } = await supabase
+  const { data, error } = await supabase
     .from('room_participants')
     .select(
       `role,
@@ -15,52 +28,64 @@ export const getFetchCalendarData = async () => {
     .eq('user_id', 'd2477fa0-d848-47df-a962-fdf0d46735c0');
 
   if (error) throw new Error(error.message);
-  if (!data || !Array.isArray(data)) return;
-  const parsed: {
-    title: string;
-    topics: {
-      title: string;
-      subtopics: {
-        title: string;
-        isDone: boolean;
-        todos: { title: string; isDone: boolean; createdAt: string }[];
-      }[];
-    }[];
-  }[] = [];
+  if (!data || !Array.isArray(data)) return [];
 
+  const processedData: ProcessedDataType = [];
+
+  // 타입 가드 함수
+  function isValidRoom(room: unknown): room is RoomFetchType {
+    return (
+      typeof room === 'object' &&
+      room !== null &&
+      'id' in room &&
+      'mandalarts' in room &&
+      Array.isArray((room as RoomFetchType).mandalarts)
+    );
+  }
+
+  // 데이터 가공 과정
+  // 핵심주제 > 대주제 > 소주제 > 투두 순의 배열 안 객체를 생성
   data.forEach((participant) => {
-    participant.rooms?.mandalarts?.forEach((mandalart) => {
-      const core = {
-        title: mandalart.title,
-        topics: [],
-      };
+    const room = participant.rooms;
 
-      mandalart.mandalart_topics?.forEach((topic) => {
-        const topicGroup = {
-          title: topic.topic,
-          subtopics: [],
+    if (isValidRoom(room)) {
+      room.mandalarts.forEach((mandalart) => {
+        const core: CoreTempType = {
+          title: mandalart.title,
+          topics: [],
         };
 
-        topic.mandalart_subtopics?.forEach((sub) => {
-          const subtopic = {
-            title: sub.content,
-            isDone: sub.is_done,
-            todos:
-              sub.cell_todos?.map((todo) => ({
+        mandalart.mandalart_topics.forEach((topic) => {
+          const topicGroup: TopicTempType = {
+            title: topic.topic,
+            subtopics: [],
+          };
+
+          topic.mandalart_subtopics.forEach((sub) => {
+            const subtopic: SubtopicTempType = {
+              title: sub.content,
+              isDone: sub.is_done,
+              todos: [],
+            };
+
+            subtopic.todos = sub.cell_todos.map(
+              (todo): TodoTempType => ({
                 title: todo.title,
                 isDone: todo.is_done,
                 createdAt: todo.created_at,
-              })) ?? [],
-          };
+              })
+            );
 
-          (topicGroup.subtopics as any[]).push(subtopic);
+            topicGroup.subtopics.push(subtopic);
+          });
+
+          core.topics.push(topicGroup);
         });
 
-        (core.topics as any[]).push(topicGroup);
+        processedData.push(core);
       });
-
-      parsed.push(core);
-    });
+    }
   });
-  return parsed;
+
+  return processedData;
 };
