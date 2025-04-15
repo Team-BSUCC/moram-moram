090a2ec3d2c79f691afcffceb78ff0d4a31f244e
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-04-15 22:38:36 +0900

    feature:#83 체크박스 추가 및 UI 개선

--- src/modules/calendar/components/calendar-floating-sheet.tsx
+++ src/modules/calendar/components/calendar-floating-sheet.tsx
@@ -3,12 +3,19 @@ import Text from '@/components/commons/text';
 import Title from '@/components/commons/title';
 import useFloatingSheetStore from '@/shared/hooks/use-floating-sheet-store';
 import { CoreType, EventType } from '../type/todo-type';
+import CheckBox from '@/components/commons/check-box';
 
 type CalendarFloatingSheetProps = {
   todos: CoreType[] | undefined;
   events: EventType[] | undefined;
 };
 
+/**
+ * @todo: 체크박스 클릭한게 DB에 반영되게 수정
+ * @param todos - 플로팅 시트에 표시할 투두 목록
+ * @param events - 달력에 표시된 이벤트 목록
+ * @returns
+ */
 const CalendarFloatingSheet = ({
   todos: data,
   events,
@@ -26,15 +33,20 @@ const CalendarFloatingSheet = ({
       <Title as='h1'>TO DO LIST</Title>
       {isSatisfied && data ? (
         data.map((core) => (
-          <div key={core.title}>
-            <Title as='h2'>{core.title}</Title>
-            {core.topics.map((topic, topicIdx) =>
-              topic.subtopics.map((sub, subIdx) =>
+          <div key={core.title} className='flex flex-col gap-3'>
+            <Title as='h2' highlightColor='bg-blue-500'>
+              {core.title}
+            </Title>
+            {core.topics.map((topic) =>
+              topic.subtopics.map((sub) =>
                 sub.todos
                   .filter((todo) => todo.createdAt.slice(0, 10) === info)
                   .map((todo, idx) => (
-                    <div key={`${topicIdx}-${subIdx}-${idx}`}>
-                      <div>{todo.title}</div>
+                    <div key={idx}>
+                      <div className='flex gap-3'>
+                        <CheckBox />
+                        <div>{todo.title}</div>
+                      </div>
                       <div className='text-gray-500 text-sm'>
                         {topic.title} &gt; {sub.title}
                       </div>
