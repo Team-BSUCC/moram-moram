d645f2dae5e4a3364edfdcf79916e34450808416
LSJ0706 <zosksldhs@gmail.com>
2025-04-16 10:48:22 +0900

    feature:#77 홈 페이지에 애니메이션 섹션 추가 및 히어로 섹션 내용 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import AnimatedSection from '@/components/landing/animated-section';
 import HeroSection from '@/components/landing/hero-section';
 import IntroSection from '@/components/landing/intro-sercion';
 import MandalartExplainSection from '@/components/landing/mandalart-explain-section';
@@ -9,16 +10,39 @@ import CTASection from '@/components/landing/cta-section';
 
 const HomePage = async () => {
   return (
-    <>
-      <HeroSection />
-      <IntroSection />
-      <MandalartExplainSection />
-      <TodoEditorSection />
-      <ProgressSection />
-      <CalendarSection />
-      <TeamPlanningSection />
-      <CTASection />
-    </>
+    <div className='scroll-snap-y h-screen w-[100dvw] snap-mandatory scroll-smooth'>
+      <AnimatedSection>
+        <HeroSection />
+      </AnimatedSection>
+
+      <AnimatedSection delay={0.1}>
+        <IntroSection />
+      </AnimatedSection>
+
+      <AnimatedSection delay={0.2}>
+        <MandalartExplainSection />
+      </AnimatedSection>
+
+      <AnimatedSection delay={0.3}>
+        <TodoEditorSection />
+      </AnimatedSection>
+
+      <AnimatedSection delay={0.4}>
+        <ProgressSection />
+      </AnimatedSection>
+
+      <AnimatedSection delay={0.5}>
+        <CalendarSection />
+      </AnimatedSection>
+
+      <AnimatedSection delay={0.6}>
+        <TeamPlanningSection />
+      </AnimatedSection>
+
+      <AnimatedSection delay={0.7}>
+        <CTASection />
+      </AnimatedSection>
+    </div>
   );
 };
 
--- src/components/landing/hero-section.tsx
+++ src/components/landing/hero-section.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import Text from '../commons/text';
+import Title from '../commons/title';
+import Spacer from '../commons/spacer';
+import Button from '../commons/button';
 
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return (
+    <div className="min-h-screen w-full bg-[url('/images/landing-img-hero.png')] bg-cover bg-center bg-no-repeat">
+      <div className='mx-auto flex h-full max-w-[1440px] items-center px-12'>
+        <div className='flex max-w-[520px] flex-col items-start gap-[56px]'>
+          <div className='gap[56px] flex w-full flex-col items-start justify-center'>
+            <Title as='h1'>목표를 구체적으로, 계획을 실행 가능하게</Title>
+            <Spacer size='lg' />
+            <Text>막연했던 목표도 차근차근, 구체적인 계획으로. </Text>
+            <Text>한 칸씩 채우며 나아가는 여정에 Manda가 함께할게요.</Text>
+            <Button>지금 시작하기</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
-
 export default HeroSection;
