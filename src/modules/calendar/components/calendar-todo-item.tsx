2501e6004afaf596fac2109ab46617c9a3b38f85
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-04-16 23:31:38 +0900

    refactor:#75 투두 컴포넌트 분리

---
+++ src/modules/calendar/components/calendar-todo-item.tsx
@@ -0,0 +1,27 @@
+import CheckBox from '@/components/commons/check-box';
+import { SubtopicType, TodoType, TopicType } from '../type/todo-type';
+
+type CalendarTodoItemProps = {
+  todo: TodoType;
+  topic: TopicType;
+  sub: SubtopicType;
+};
+
+const CalendarTodoItem = ({ todo, topic, sub }: CalendarTodoItemProps) => {
+  return (
+    <div>
+      <div className='flex items-start justify-between'>
+        <div className='flex gap-3'>
+          <CheckBox />
+          <div className='font-medium'>{todo.title}</div>
+        </div>
+        <button className='text-gray-500 px-2 text-xl leading-none'>⋮</button>
+      </div>
+      <div className='text-gray-500 ml-9 mt-1 text-sm'>
+        {topic.title} &gt; {sub.title}
+      </div>
+    </div>
+  );
+};
+
+export default CalendarTodoItem;
