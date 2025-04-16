789acd0ae338a7f9e0e66c8946f4c9ddd70fc607
KGyeong <rlarudals61@naver.com>
2025-04-16 16:26:00 +0900

    feature:#86 만다라트 정보 카드 컴포넌트 제작

--- src/modules/dashboard/components/mandalart-card.tsx
+++ src/modules/dashboard/components/mandalart-card.tsx
@@ -1,39 +1,63 @@
 import CircularProgress from '@/components/commons/circular-progress';
 import Spacer from '@/components/commons/spacer';
 import CardButtonDropDown from './card-button-drop-down';
+import Link from 'next/link';
+import { Avatar } from '@/components/ui/avatar';
+import Text from '@/components/commons/text';
+import Title from '@/components/commons/title';
+import { calculatorProgress } from '@/shared/utils/calculator-progress';
+import { formatDate } from '@/shared/utils/format-date';
+import { MandalartType } from '@/modules/mandalart/types/realtime-type';
 
 type MandalartCardProps = {
+  info: MandalartType & { done_count: number };
   bandColor: string;
 };
 
-const MandalartCard = ({ bandColor }: MandalartCardProps) => {
+const MandalartCard = ({ info, bandColor }: MandalartCardProps) => {
   return (
-    <div className={`${bandColor} w-fit rounded-br-lg shadow-md`}>
-      <div className='ml-3 w-96 rounded-br-lg bg-white-light'>
-        <div className='flex-col p-3'>
-          <div className='flex justify-between'>
-            <div>365남음/날짜</div>
-            <CardButtonDropDown />
-          </div>
-          <div className='flex-col'>
-            <p>성장의 해</p>
-            <p>완수한다!</p>
-            <p>avatar</p>
-          </div>
-          <Spacer size='sm' />
-          <hr className='' />
-          <Spacer size='sm' />
-          <div className='flex gap-3'>
-            <div>
-              <CircularProgress value={50} size='sm' />
+    <div
+      className={`${bandColor} grid w-full rounded-br-lg rounded-tr-lg shadow-md`}
+    >
+      <Link href={`/room/${info.id}`}>
+        <div className='ml-3 rounded-br-lg rounded-tr-lg bg-white-light'>
+          <div className='w-full flex-col p-3'>
+            <div className='flex justify-between'>
+              <div className='flex gap-2'>
+                <Text variant='default' size='md' align='left'>
+                  365일 남음
+                </Text>
+                <span className='mb-[1px] self-end text-ss text-gray'>
+                  {formatDate(info.created_at)}
+                </span>
+              </div>
+              <CardButtonDropDown />
             </div>
-            <div className='flex-col place-content-center'>
-              <p>지금까지</p>
-              <p>50% 해냈어요</p>
+            <div className='flex-col'>
+              <Title variant='default' as='h4' size='default'>
+                {info.title}
+              </Title>
+              <p>완수한다!</p>
+              <Avatar />
+            </div>
+            <Spacer size='sm' />
+            <hr className='w-full' />
+            <Spacer size='sm' />
+            <div className='flex gap-3'>
+              <div>
+                <CircularProgress
+                  value={calculatorProgress(info.done_count)}
+                  size='sm'
+                />
+              </div>
+              <div className='flex-col place-content-center'>
+                <p>지금까지</p>
+                <p>{calculatorProgress(info.done_count)}% 해냈어요</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
