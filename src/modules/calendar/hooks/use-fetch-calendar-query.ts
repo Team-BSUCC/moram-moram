43b73a94fd1343967a02ae72ace54c814ea46721
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-04-16 14:54:16 +0900

    refactor:#83 my-mandalarts API 추가 및 공용 폴더로 이동

--- src/modules/calendar/hooks/use-fetch-calendar-query.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { getAllMandalartData } from '../../../shared/api/get-all-mandalart-data';
-
-export const useFetchCalendarQuery = () => {
-  return useQuery({
-    queryKey: ['my-mandalarts'],
-    queryFn: async () => await getAllMandalartData(),
-  });
-};
