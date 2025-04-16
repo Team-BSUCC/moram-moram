b3aadc3cc32456663607a7ca2afbfa4fa8e9a1e2
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-04-16 12:27:51 +0900

    refactor:#93 투두가 없을 경우 박스가 보이지 않게 수정

--- src/app/today-list/page.tsx
+++ src/app/today-list/page.tsx
@@ -7,9 +7,12 @@ import Title from '@/components/commons/title';
 import { useFetchCalendarQuery } from '@/modules/calendar/hooks/use-fetch-calendar-query';
 import { useState } from 'react';
 
+/**
+ * @todo: 처음 들어갔을 때 배열의 맨 앞 만다라트의 투두가 보이도록 수정하기
+ * @todo: 필터링 기능 추가(셀렉트 박스)
+ */
 const TodayListPage = () => {
   const { data, isPending } = useFetchCalendarQuery();
-  // 초기값으로 가장 앞의 만다라트 제목 넣기
   const [clickedTitle, setClickedTitle] = useState<string>('');
 
   if (isPending) {
@@ -25,7 +28,7 @@ const TodayListPage = () => {
       <Spacer size={'2xl'} />
       <Title as='h1'>투두 모아보기</Title>
       <Text>내 만다라트 별 TO DO LIST를 한번에 확인하세요.</Text>
-      <Spacer size={'xl'} />
+      <Spacer size={'lg'} />
       {/* 투두 목록을 표시할 만다라트 선택창 */}
       <div className='mb-5 flex gap-2'>
         {data?.map((topic, idx) => (
@@ -42,34 +45,35 @@ const TodayListPage = () => {
           </Button>
         ))}
       </div>
-
-      {/* 셀렉트 박스 */}
-      <div className='flex items-end'>
+      {/* 셀렉트 박스 (기능 추가 예정) */}
+      <div>
         <select name='' id=''>
           <option value=''>남은 할 일</option>
           <option value=''>완료한 일</option>
           <option value=''>전체 보기</option>
         </select>
       </div>
-
       {/* 할 일 목록 */}
       <div className='w-full'>
         <div className='mt-6 w-full space-y-4'>
           <div className='mt-6 w-full space-y-6'>
             {data
               ?.find((mandalart) => mandalart.title === clickedTitle)
-              ?.topics.map((topic, topicIdx) => (
+              ?.topics.filter((topic) =>
+                topic.subtopics.some((sub) => sub.todos.length > 0)
+              )
+              .map((topic, topicIdx) => (
                 <div
                   key={topicIdx}
-                  className='bg-gray-50 w-full border-l-4 border-yellow-400 p-6'
+                  className='bg-gray-50 w-full border-l-8 border-yellow-400 p-6'
                 >
                   <Title as='h3'>{topic.title}</Title>
-                  <Spacer size={'xl'} />
+                  <Spacer size={'md'} />
                   <div className='space-y-4'>
                     {topic.subtopics.map((sub, subIdx) =>
                       sub.todos.map((todo, todoIdx) => (
                         <div key={`${subIdx}-${todoIdx}`} className='space-y-1'>
-                          <div className='w-full border-b-[1px] border-[#D2D2D2] font-semibold'>
+                          <div className='w-full border-b-[1px] border-[#D2D2D2] pb-2 text-lg font-semibold'>
                             {todo.title}
                           </div>
                           <div className='text-sm text-[#5E5E5E]'>
