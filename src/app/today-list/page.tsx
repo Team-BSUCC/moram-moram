21e31f10c2867ee50c7362710f6810af43543401
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-04-17 10:45:23 +0900

    style:#92 투두 모아보기 페이지 스타일 수정

--- src/app/today-list/page.tsx
+++ src/app/today-list/page.tsx
@@ -26,99 +26,114 @@ const TodayListPage = () => {
   if (isPending) return <div>Loading...</div>;
 
   return (
-    <div className='flex w-full flex-col items-start'>
-      <Spacer size={'2xl'} />
-      <Title as='h1'>투두 모아보기</Title>
-      <Text>내 만다라트 별 TO DO LIST를 한번에 확인하세요.</Text>
-      <Spacer size={'lg'} />
+    <div className='h-full w-full bg-white-dark'>
+      <div className='mx-auto flex w-2/3 flex-col items-start'>
+        <Spacer size={'2xl'} />
+        <Title as='h1'>투두 모아보기</Title>
+        <Text>내 만다라트 별 TO DO LIST를 한번에 확인하세요.</Text>
+        <Spacer size={'lg'} />
 
-      {/* 만다라트 제목 탭 */}
-      <div className='mb-5 flex gap-4'>
-        {data?.map((topic, idx) => (
-          <Button key={idx} onClick={() => handleClick(topic.title)}>
-            <div
-              className={
-                topic.title === clickedTitle
-                  ? 'border-b-[2px] border-[#000]'
-                  : 'border-b-[2px] border-[#fff]'
-              }
+        {/* 만다라트 제목 탭 */}
+        <div className='mb-5 flex gap-4'>
+          {data?.map((topic, idx) => (
+            <Button
+              key={idx}
+              variant={'outline'}
+              onClick={() => handleClick(topic.title)}
             >
-              <Title as='h2'>{topic.title}</Title>
-            </div>
-          </Button>
-        ))}
-      </div>
-
-      {/* 셀렉트 박스 */}
-      <div>
-        <select
-          name='todoListView'
-          value={selectedOption}
-          onChange={(e) => setSelectedOption(e.target.value)}
-        >
-          <option value='left'>남은 할 일</option>
-          <option value='done'>완료한 일</option>
-          <option value='all'>전체 보기</option>
-        </select>
-      </div>
-
-      {/* 투두 출력 */}
-      <div className='w-full'>
-        <div className='mt-6 w-full space-y-4'>
-          {data
-            ?.find((mandalart) => mandalart.title === clickedTitle)
-            ?.topics.filter((topic) =>
-              topic.subtopics.some((sub) =>
-                sub.todos.some((todo) => {
-                  if (selectedOption === 'left') return !todo.isDone;
-                  if (selectedOption === 'done') return todo.isDone;
-                  return true;
-                })
-              )
-            )
-            .map((topic, topicIdx) => (
               <div
-                key={topicIdx}
-                className='bg-gray-50 w-full border-l-8 border-yellow-400 p-6'
+                className={
+                  topic.title === clickedTitle
+                    ? 'border-b-[2px] border-[#000]'
+                    : 'border-b-[2px] border-[#fff]'
+                }
               >
-                <Title as='h3'>{topic.title}</Title>
-                <Spacer size={'md'} />
-                <div className='space-y-6'>
-                  {topic.subtopics
-                    .filter((sub) =>
-                      sub.todos.some((todo) => {
-                        if (selectedOption === 'left') return !todo.isDone;
-                        if (selectedOption === 'done') return todo.isDone;
-                        return true;
-                      })
-                    )
-                    .map((sub, subIdx) => (
-                      <div key={subIdx}>
-                        <Title as='h4' highlightColor='bg-red-500'>
-                          {sub.title}
-                        </Title>
-                        <Spacer size={'md'} />
-                        {sub.todos
-                          .filter((todo) => {
-                            if (selectedOption === 'left') return !todo.isDone;
-                            if (selectedOption === 'done') return todo.isDone;
-                            return true;
-                          })
-                          .map((todo, todoIdx) => (
-                            <div key={todoIdx} className='ml-1 mt-3 space-y-1'>
-                              <div className='w-full border-b border-[#D2D2D2] pb-1 text-[16px] font-medium'>
-                                {todo.title}
-                              </div>
-                              <div className='text-sm text-[#5E5E5E]'>
-                                {todo.createdAt.slice(0, 10)}
-                              </div>
-                            </div>
-                          ))}
-                      </div>
-                    ))}
-                </div>
+                <Title as='h2'>{topic.title}</Title>
               </div>
-            ))}
+            </Button>
+          ))}
+        </div>
+
+        {/* 셀렉트 박스 */}
+        <div className='flex w-full justify-end'>
+          <select
+            name='todoListView'
+            value={selectedOption}
+            onChange={(e) => setSelectedOption(e.target.value)}
+            className='bg-transparent'
+          >
+            <option value='left'>남은 할 일</option>
+            <option value='done'>완료한 일</option>
+            <option value='all'>전체 보기</option>
+          </select>
+        </div>
+
+        {/* 투두 출력 */}
+        <div className='w-full'>
+          <div className='mt-6 w-full space-y-4'>
+            {data
+              ?.find((mandalart) => mandalart.title === clickedTitle)
+              ?.topics.filter((topic) =>
+                topic.subtopics.some((sub) =>
+                  sub.todos.some((todo) => {
+                    if (selectedOption === 'left') return !todo.isDone;
+                    if (selectedOption === 'done') return todo.isDone;
+                    return true;
+                  })
+                )
+              )
+              .map((topic, topicIdx) => (
+                <div
+                  key={topicIdx}
+                  className='w-full border-l-8 border-yellow-pigment bg-white-light p-6'
+                >
+                  <p className='text-[24px] font-semibold'>{topic.title}</p>
+                  <Spacer size={'md'} />
+                  <div className='space-y-6'>
+                    {topic.subtopics
+                      .filter((sub) =>
+                        sub.todos.some((todo) => {
+                          if (selectedOption === 'left') return !todo.isDone;
+                          if (selectedOption === 'done') return todo.isDone;
+                          return true;
+                        })
+                      )
+                      .map((sub, subIdx) => (
+                        <div key={subIdx}>
+                          <p className='border-l-4 border-[#666666] px-3 text-[18px]'>
+                            {sub.title}
+                          </p>
+                          <Spacer size={'md'} />
+                          <div className='flex flex-col gap-[20px]'>
+                            {sub.todos
+                              .filter((todo) => {
+                                if (selectedOption === 'left')
+                                  return !todo.isDone;
+                                if (selectedOption === 'done')
+                                  return todo.isDone;
+                                return true;
+                              })
+                              .map((todo, todoIdx) => (
+                                <div
+                                  key={todoIdx}
+                                  className='ml-1 mt-3 space-y-1'
+                                >
+                                  <div className='flex w-full items-center justify-between border-b border-[#D2D2D2] pb-1 text-[20px] font-medium'>
+                                    {todo.title}
+                                    <div>⋮</div>
+                                  </div>
+                                  <div className='text-[16px] text-[#5E5E5E]'>
+                                    {todo.createdAt.slice(0, 10)}
+                                  </div>
+                                </div>
+                              ))}
+                          </div>
+                        </div>
+                      ))}
+                  </div>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
     </div>
