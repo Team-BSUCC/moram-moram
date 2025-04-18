9b0357cf7c7d417625b1230e83863345a7d81528
KGyeong <rlarudals61@naver.com>
2025-04-19 02:49:00 +0900

    chore:#58 필요에 맞게 공통 컴포넌트에 스타일 추가

--- src/components/commons/title.tsx
+++ src/components/commons/title.tsx
@@ -19,16 +19,22 @@ export const titleVariants = cva('flex gap-2 transition-all', {
         'text-[24px] leading-[36px] font-normal sm:text-[28px] sm:leading-[42px] md:text-[32px] md:leading-[48px]',
       '32px-medium':
         'text-[24px] leading-[36px] font-medium sm:text-[28px] sm:leading-[42px] md:text-[32px] md:leading-[48px]',
+      '28px-semibold':
+        'text-[20px] leading-[30px] font-semibold sm:text-[24px] sm:leading-[36px] md:text-[28px] md:leading-[42px]',
       '28px-regular':
         'text-[20px] leading-[30px] font-normal sm:text-[24px] sm:leading-[36px] md:text-[28px] md:leading-[42px]',
       '24px-semibold':
         'text-[20px] leading-[28px] font-semibold sm:text-[22px] sm:leading-[30px] md:text-[24px] md:leading-[32.4px]',
+      '24px-regular':
+        'text-[18px] leading-[27px] font-normal sm:text-[20px] sm:leading-[30px] md:text-[24px] md:leading-[36px]',
       '20px-bold':
         'text-[16px] leading-[24px] font-bold sm:text-[18px] sm:leading-[27px] md:text-[20px] md:leading-[30px]',
       '20px-medium':
         'text-[16px] leading-[24px] font-medium sm:text-[18px] sm:leading-[27px] md:text-[20px] md:leading-[30px]',
       '20px-regular':
         'text-[16px] leading-[24px] font-normal sm:text-[18px] sm:leading-[27px] md:text-[20px] md:leading-[30px]',
+      '18px-semibold':
+        'text-[14px] leading-[20px] font-semibold sm:text-[16px] sm:leading-[24px] md:text-[18px] md:leading-[27px]',
       '18px-medium':
         'text-[14px] leading-[20px] font-medium sm:text-[16px] sm:leading-[24px] md:text-[18px] md:leading-[27px]',
       '16px-semibold':
