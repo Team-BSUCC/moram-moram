abd7e35deed7b7dc394fa7d4f02d784178d582aa
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-04-19 02:10:38 +0900

    feature:#75 플로팅 시트에 요일 추가

--- src/modules/calendar/utils/get-processed-date.ts
+++ src/modules/calendar/utils/get-processed-date.ts
@@ -1,3 +1,5 @@
 export const getProcessedDate = (date: string) => {
-  return date.replaceAll('-', '.');
+  const daysKor = ['일', '월', '화', '수', '목', '금', '토'];
+  const dayOfWeek = daysKor[new Date(date).getDay()];
+  return date.replaceAll('-', '.') + ' ' + dayOfWeek;
 };
