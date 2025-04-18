3e61824a8c5f3faa880c1e8c844967944fade8fd
KGyeong <rlarudals61@naver.com>
2025-04-19 02:49:47 +0900

    feature:#58 컴포넌트 제작

--- src/modules/guest/components/guest-mandalart-block.tsx
+++ src/modules/guest/components/guest-mandalart-block.tsx
@@ -3,33 +3,55 @@
 import { getColorWithNumber } from '@/shared/utils/get-color-with-number';
 import { useGuestTopicStore } from '../hooks/use-guest-topic-store';
 import GuestMandalartCell from './guest-mandalart-cell';
+import { useEffect, useRef } from 'react';
+import { delayWithIndex } from '../util/delay-with-index';
 
 type GuestMandalartBlockProps = {
   index: number;
 };
 
 const GuestMandalartBlock = ({ index }: GuestMandalartBlockProps) => {
+  const coreValue = useGuestTopicStore((state) => state.core);
   const value = useGuestTopicStore((state) => state.topics[index]);
   const setValue = useGuestTopicStore((state) => state.setTopic);
+  const topicRef = useRef<HTMLTextAreaElement>(null);
+  const MIN_LINE_HEIGHT = 24;
+
+  const active = !coreValue;
+
+  const activeSub = !value;
+
+  useEffect(() => {
+    const el = topicRef.current;
+    if (!el) return;
+
+    el.style.height = 'auto'; // 초기화
+    const newHeight = Math.max(el.scrollHeight, MIN_LINE_HEIGHT);
+    el.style.height = `${newHeight}px`; // 최소 높이 보장
+  }, [value]);
   return (
     <div className='grid aspect-square grid-cols-3 grid-rows-3 gap-2'>
       <div
-        className={`${getColorWithNumber(index)} relative col-start-2 row-start-2 flex aspect-square max-w-full items-center justify-center overflow-hidden rounded-lg border-2 p-2`}
+        className={`${!active ? getColorWithNumber(index) : 'bg-lightgray'} relative col-start-2 row-start-2 flex aspect-square max-w-full items-center justify-center overflow-hidden rounded-lg border-[3px] border-main p-2 transition-all delay-100 duration-500`}
       >
         <textarea
-          className='overflow-hidden rounded-md border-none bg-transparent p-2 text-center outline-none'
+          disabled={active}
+          ref={topicRef}
+          className='text-16px mt-1 min-h-[24px] w-full max-w-md resize-none overflow-hidden break-keep bg-transparent text-center font-semibold leading-[1.5] outline-none placeholder:text-sub'
           value={value}
           onChange={(e) => {
             setValue(index, e.target.value);
           }}
+          placeholder='대주제'
+          rows={1}
         />
       </div>
       {Array.from({ length: 8 }).map((_, index) => (
         <div
           key={index}
-          className='relative flex aspect-square max-w-full items-center justify-center overflow-hidden rounded-lg border p-2'
+          className={`${!activeSub ? 'bg-white-light' : 'bg-lightgray'} ${delayWithIndex(index)} relative flex aspect-square max-w-full items-center justify-center overflow-hidden rounded-lg border border-caption p-2 transition-all delay-[400ms] duration-500`}
         >
-          <GuestMandalartCell key={index} />
+          <GuestMandalartCell key={index} active={activeSub} />
         </div>
       ))}
     </div>
--- src/modules/guest/components/guest-mandalart-cell.tsx
+++ src/modules/guest/components/guest-mandalart-cell.tsx
@@ -1,13 +1,29 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-const GuestMandalartCell = () => {
+const GuestMandalartCell = ({ active }: { active: boolean }) => {
   const [value, setValue] = useState<string>('');
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const MIN_LINE_HEIGHT = 24;
+
+  useEffect(() => {
+    const el = textareaRef.current;
+    if (!el) return;
+
+    el.style.height = 'auto';
+    const newHeight = Math.max(el.scrollHeight, MIN_LINE_HEIGHT);
+    el.style.height = `${newHeight}px`;
+  }, [value]);
+
   return (
     <textarea
-      className='overflow-hidden rounded-md border border-none bg-transparent p-2 text-center outline-none'
+      disabled={active}
+      ref={textareaRef}
       value={value}
       onChange={(e) => setValue(e.target.value)}
+      className='text-16px mt-1 min-h-[24px] w-full max-w-md resize-none overflow-hidden break-keep bg-transparent text-center font-regular leading-[1.5] outline-none placeholder:text-caption'
+      placeholder='실행 방안'
+      rows={1}
     />
   );
 };
