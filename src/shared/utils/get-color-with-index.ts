5a3db80e3ef3761c7446f74cf71f84b1ee865c6c
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-04-21 14:51:16 +0900

    chore:#75 색생코드 변수로 변경

--- src/shared/utils/get-color-with-index.ts
+++ src/shared/utils/get-color-with-index.ts
@@ -6,14 +6,14 @@
 export const getPigmentCodeWithIndex = (index: number): string => {
   const idx = index % 8;
   const colorCode: Record<number, string> = {
-    0: '#F6B2BB',
-    1: '#F99695',
-    2: '#FB936A',
-    3: '#F4EB6C',
-    4: '#AAD676',
-    5: '#9CC6E8',
-    6: '#73B0DD',
-    7: '#8E93F0',
+    0: 'var(--color-pink-pigment)',
+    1: 'var(--color-red-pigment)',
+    2: 'var(--color-orange-pigment)',
+    3: 'var(--color-yellow-pigment)',
+    4: 'var(--color-green-pigment)',
+    5: 'var(--color-sky-pigment)',
+    6: 'var(--color-blue-pigment)',
+    7: 'var(--color-purple-pigment)',
   };
   return colorCode[idx];
 };
@@ -26,14 +26,14 @@ export const getPigmentCodeWithIndex = (index: number): string => {
 export const getPastelCodeWithIndex = (index: number): string => {
   const idx = index % 8;
   const colorCode: Record<number, string> = {
-    0: '#FBDEE2',
-    1: '#FCC8C7',
-    2: '#FDCDBA',
-    3: '#FBF8CB',
-    4: '#DAEDC3',
-    5: '#E5F0F9',
-    6: '#C5DEF1',
-    7: '#D1D3F9',
+    0: 'var(--color-pink-pastel)',
+    1: 'var(--color-red-pastel)',
+    2: 'var(--color-orange-pastel)',
+    3: 'var(--color-yellow-pastel)',
+    4: 'var(--color-green-pastel)',
+    5: 'var(--color-sky-pastel)',
+    6: 'var(--color-blue-pastel)',
+    7: 'var(--color-purple-pastel)',
   };
   return colorCode[idx];
 };
