74abba7fb4565ddf10cb5c4441a8cd3d4ce03506
Taejin7088 <gog70888@gmail.com>
2025-04-21 17:31:39 +0900

    feature:#139 RPC사용해서 룸 오너랑 초대된  사용자 바로 접근 가능하게 기능추가, 유저 정보 플로팅 시트 패스파라미터기준으로 유저정보 가져오게 수정

--- src/app/mandalart/[id]/page.tsx
+++ src/app/mandalart/[id]/page.tsx
@@ -1,33 +1,33 @@
 import { getUserInfo } from '@/modules/auth/services/auth-server-service';
 import MandalartMainContent from '@/modules/mandalart/components/mandalart-main-content';
 import MandalartPasscodeGate from '@/modules/mandalart/components/mandalart-passcode-gate';
-
-import { Tables } from '@/shared/types/database.types';
 import { getServerClient } from '@/shared/utils/supabase/server-client';
+import { PostgrestError } from '@supabase/supabase-js';
 
 type MandalartPageType = {
   params: { id: string };
+};
+
+type FetchDataType = {
+  data: { ownerId: string; mandalartId: string; participants: string[] };
+  error: PostgrestError | null;
 };
 
 const MandalartPage = async ({ params }: MandalartPageType) => {
   const user = await getUserInfo();
   const supabase = getServerClient();
 
-  //TODO 동적으로 pathPram 가져오는 걸로 수정
   const pathParamRoomId = params.id;
-  const { data, error } = await supabase
-    .from('rooms')
-    .select('*')
-    .eq('id', pathParamRoomId);
+  const { data, error } = (await supabase.rpc('fetch_room_data_for_mandalart', {
+    room_uuid: pathParamRoomId,
+  })) as FetchDataType;
 
   if (error) throw error;
-  const roomInfo: Tables<'rooms'> = data[0];
 
-  //TODO path로 가져온 룸ID를 기준으로 가져오는걸로 수정
-  const mandalartId = '6424de9b-7fbf-470a-9743-c9bb5e3cdad8';
+  const isAuthenticated =
+    data.ownerId === user?.id || data.participants.includes(user?.id ?? '');
 
-  //TODO 조인으로 파티셔널까지 다 긁어와서 비교 유저 id랑 하나라도 일치하면 만다라트 컴포넌트 렌더링
-  const isAuthenticated = roomInfo.owner === user?.id;
+  const mandalartId = data.mandalartId;
   return (
     <>
       {isAuthenticated ? (
