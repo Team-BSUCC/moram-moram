854f17c365ee67f1aeddfa1b1937d24a30f18160
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-04-22 00:26:44 +0900

    refactor:#144 헤더 중복 Link 생성 코드 개선

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 import URLS from '@/shared/constants/url-constants';
 import { User } from '@supabase/supabase-js';
 import Image from 'next/image';
@@ -17,9 +18,24 @@ const Header = ({ user }: HeaderProps) => {
 
   const userName = getCurrentUserName(user);
 
+  const menuItems = user
+    ? [
+        { to: URLS.DASHBOARD, label: '내 만다라트', className: 'bg-[#F0E9E5]' },
+        { to: URLS.CALENDAR, label: '캘린더', className: 'bg-[#F0E9E5]' },
+        { to: URLS.TODAY_LIST, label: '투두리스트', className: 'bg-[#F0E9E5]' },
+      ]
+    : [
+        { to: URLS.SIGN_IN, label: '로그인', className: 'bg-[#F0E9E5]' },
+        { to: URLS.SIGN_UP, label: '회원가입', className: 'bg-[#F0E9E5]' },
+        {
+          to: URLS.GUEST,
+          label: '비회원으로 체험하기',
+          className: 'border-2 border-[#F0E9E5]',
+        },
+      ];
+
   return (
     <div className='h-full w-full border-b bg-white-light'>
-      {/* <Header /> 컴포넌트가 들어올 예정입니다. */}
       <div className='flex h-full items-center justify-between'>
         <div className='p-8'>
           <Image
@@ -27,29 +43,21 @@ const Header = ({ user }: HeaderProps) => {
             alt='만다로고'
             width='166'
             height='48'
-          ></Image>
+          />
         </div>
         <div className='flex gap-4 pr-8'>
-          {user ? (
+          {menuItems.map((item, index) => (
+            <Link
+              key={index}
+              href={item.to}
+              className={`${linkStyle} ${item.className}`}
+            >
+              {item.label}
+            </Link>
+          ))}
+
+          {user && (
             <>
-              <Link
-                href={URLS.DASHBOARD}
-                className={`${linkStyle} bg-[#F0E9E5]`}
-              >
-                내 만다라트
-              </Link>
-              <Link
-                href={URLS.CALENDAR}
-                className={`${linkStyle} bg-[#F0E9E5]`}
-              >
-                캘린더
-              </Link>
-              <Link
-                href={URLS.TODAY_LIST}
-                className={`${linkStyle} bg-[#F0E9E5]`}
-              >
-                투두리스트
-              </Link>
               <SignOutButton />
               <div className='flex gap-2 pl-2'>
                 <Avatar className='border border-black hover:z-10'>
@@ -58,21 +66,6 @@ const Header = ({ user }: HeaderProps) => {
                 </Avatar>
                 <p className='text-xl'>{userName}</p>
               </div>
-            </>
-          ) : (
-            <>
-              <Link href={URLS.SIGN_IN} className={`${linkStyle} bg-[#F0E9E5]`}>
-                로그인
-              </Link>
-              <Link href={URLS.SIGN_UP} className={`${linkStyle} bg-[#F0E9E5]`}>
-                회원가입
-              </Link>
-              <Link
-                href={URLS.GUEST}
-                className={`${linkStyle} border-2 border-[#F0E9E5]`}
-              >
-                비회원으로 체험하기
-              </Link>
             </>
           )}
         </div>
