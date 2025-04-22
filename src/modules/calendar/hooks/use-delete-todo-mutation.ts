7afd826fe58487dfaa0a93484c740c4dccd8d0c8
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-04-22 10:55:10 +0900

    feature:#92 센트리를 이용한 에러 처리 추가

--- src/modules/calendar/hooks/use-delete-todo-mutation.ts
+++ src/modules/calendar/hooks/use-delete-todo-mutation.ts
@@ -2,6 +2,8 @@ import { useMutation } from '@tanstack/react-query';
 import { useTransition } from 'react';
 import { useRouter } from 'next/navigation';
 import { deleteTodoData } from '../services/edit-todo-data';
+import * as Sentry from '@sentry/nextjs';
+import Swal from 'sweetalert2';
 
 export const useDeleteTodoMutation = () => {
   const [isPending, startTransition] = useTransition();
@@ -12,6 +14,22 @@ export const useDeleteTodoMutation = () => {
     onSuccess: () => {
       startTransition(() => {
         router.refresh();
+      });
+    },
+    onError: (error) => {
+      Sentry.withScope((scope) => {
+        scope.setTag('page', 'TodayList & Calendar Page');
+        scope.setTag('feature', 'deleteTodo');
+
+        Sentry.captureException(
+          new Error(
+            `[deleteTodo] ${error instanceof Error ? error.message : 'Unknown error'}`
+          )
+        );
+      });
+      Swal.fire({
+        icon: 'error',
+        title: '삭제를 실패했습니다. 다시 시도해주세요!',
       });
     },
   });
