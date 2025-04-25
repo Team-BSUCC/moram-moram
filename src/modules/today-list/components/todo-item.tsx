4a44cb6ff5cdd8594471480a8deff969d5c3732c
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-04-25 12:06:51 +0900

    wip:#197 테스트를 위한 임시저장

--- src/modules/today-list/components/todo-item.tsx
+++ src/modules/today-list/components/todo-item.tsx
@@ -4,23 +4,34 @@ import Text from '@/components/commons/text';
 import { useDeleteTodoMutation } from '@/modules/calendar/hooks/use-delete-todo-mutation';
 import { changeDateSeparator } from '@/modules/calendar/utils/get-processed-date';
 import { FlatTodo } from '../types/today-list-type';
+import { useState } from 'react';
+import CheckBox from '@/components/commons/check-box';
+import { useUpdateTodoMutation } from '@/modules/calendar/hooks/use-update-todo-mutation';
 
 type TodoItemProps = {
   todo: FlatTodo;
 };
 
 const TodoItem = ({ todo }: TodoItemProps) => {
-  const { mutate: deleteTodoData } = useDeleteTodoMutation();
+  const [checked, setChecked] = useState<boolean>(todo.isDone);
+  const { mutate: updateTodoMutate } = useUpdateTodoMutation();
+  const { mutate: deleteTodoMutate } = useDeleteTodoMutation();
 
   const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation();
     e.preventDefault();
-    deleteTodoData(todo.todoId);
+    deleteTodoMutate(todo.todoId);
+  };
+
+  const handleCheckToggle = () => {
+    updateTodoMutate(todo);
+    setChecked(!checked);
   };
 
   return (
     <div className='space-y-1'>
       <div className='flex w-full items-center justify-between border-b border-stroke px-1 py-2'>
+        <CheckBox checked={checked} onChange={handleCheckToggle} />
         <Text size='20px-medium'>{todo.todoTitle}</Text>
         <Dropdown>
           <div>
