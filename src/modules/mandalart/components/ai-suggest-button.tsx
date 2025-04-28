2577c2140352c8b1ec730ae46711abfaad8b3c71
Taejin7088 <gog70888@gmail.com>
2025-04-28 19:54:06 +0900

    feature:#209 추천하기 버튼을 누르면 알럿창이 뜨고 해당 알럿창에서 요청, 횟수, 로딩 상태에 따른 버튼 disable기능 구현

--- src/modules/mandalart/components/ai-suggest-button.tsx
+++ src/modules/mandalart/components/ai-suggest-button.tsx
@@ -1,14 +1,18 @@
+/* eslint-disable indent */
+
 import Button from '@/components/commons/button';
-import React from 'react';
 import { useGetAiUsageCountQuery } from '../hooks/use-get-ai-usage-count-query';
 import { useUsageCountMutations } from '../hooks/use-usage-count-mutations';
-import { infoAlert } from '@/shared/utils/sweet-alert';
 import { useClientStateStore } from '../hooks/use-client-state-store';
 import Image from 'next/image';
 import { fetchGetAiSuggestKeywords } from '../services/fetch-get-ai-suggest-keywords';
 import { useChannelStore } from '../hooks/use-channel-store';
 import { useCellBroadcastMutation } from '../hooks/use-cell-broadcast-mutation';
 import useTodoFloatingSheetStore from '../hooks/use-todo-floating-sheet-store';
+import { useAlertStore } from '@/shared/hooks/use-alert-store';
+import { LoaderCircle } from 'lucide-react';
+import { useState } from 'react';
+import { errorAlert, infoAlert } from '@/shared/utils/sweet-alert';
 
 type AiSuggestButtonProps = {
   value: string;
@@ -25,31 +29,47 @@ const AiSuggestButton = ({ value, type }: AiSuggestButtonProps) => {
   const topics = useClientStateStore((state) => state.topics);
   const subTopics = useClientStateStore((state) => state.subTopics);
 
+  const openAlert = useAlertStore((store) => store.openAlert);
+  const [isAiFetching, setIsFetching] = useState<boolean>(false);
+
   const info = useTodoFloatingSheetStore((state) => state.info);
   if (info === undefined || info === null) {
     return <div>오류</div>;
   }
 
   const MAX_AI_USAGE_COUNT = 30;
+  const isAiLimited =
+    aiUsageCount === null || aiUsageCount >= MAX_AI_USAGE_COUNT;
 
+  const topicList = Array.from(topics.values());
+  //ai api에 요청할 대주제 배열 생성
+  const topicValueList = topicList
+    .filter((topicValue) => topicValue.topic)
+    .map((topicValue) => topicValue.topic);
+
+  const subtopicList = Array.from(
+    subTopics
+      .values()
+      .filter((subtopicValue) => subtopicValue.topicId === info.id)
+  );
+  //ai api에 요청할 소주제 배열 생성
+  const subtopicValueList = subtopicList
+    .filter((subtopicValue) => subtopicValue.content)
+    .map((subtopicValue) => subtopicValue.content);
+
+  const isFullMainBlock = topicValueList.length === 8;
+  const isFullSubBlock = topicValueList.length === 8;
+
+  //대주제들을 추천받는 핸들러
   const handleAiTopicSuggest = async () => {
-    if (aiUsageCount === null || aiUsageCount >= MAX_AI_USAGE_COUNT) {
-      infoAlert('사용할 수 없습니다.');
+    if (isAiLimited) {
       return;
     }
-
-    const topicList = Array.from(topics.values());
-    const topicValueList = topicList
-      .filter((topicValue) => topicValue.topic)
-      .map((topicValue) => topicValue.topic);
-
     const aiSuggestTopicList = await fetchGetAiSuggestKeywords(
       value,
       topicValueList as string[]
     );
-
     usageCountPlus();
-
     topicList.forEach((topicValue) => {
       if (!topicValue.topic) {
         mutationCell({
@@ -60,28 +80,16 @@ const AiSuggestButton = ({ value, type }: AiSuggestButtonProps) => {
     });
   };
 
+  //소주제들을 추천받는 핸들러
   const handleAiSubTopicSuggest = async () => {
-    if (aiUsageCount === null || aiUsageCount >= MAX_AI_USAGE_COUNT) {
-      infoAlert('사용할 수 없습니다.');
+    if (isAiLimited) {
       return;
     }
-
-    const subtopicList = Array.from(
-      subTopics
-        .values()
-        .filter((subtopicValue) => subtopicValue.topicId === info.id)
-    );
-    const subtopicValueList = subtopicList
-      .filter((subtopicValue) => subtopicValue.content)
-      .map((subtopicValue) => subtopicValue.content);
-
     const aiSuggestTopicList = await fetchGetAiSuggestKeywords(
       value,
       subtopicValueList as string[]
     );
-
     usageCountPlus();
-
     subtopicList.forEach((subtopicValue) => {
       if (!subtopicValue.content) {
         mutationCell({
@@ -95,24 +103,96 @@ const AiSuggestButton = ({ value, type }: AiSuggestButtonProps) => {
     });
   };
 
+  const handleSuggest = async (result: boolean) => {
+    setIsFetching(true);
+    try {
+      if (result) {
+        switch (type) {
+          case 'core': {
+            await handleAiTopicSuggest();
+            break;
+          }
+          case 'topic':
+            await handleAiSubTopicSuggest();
+            break;
+        }
+      }
+    } catch (error) {
+      errorAlert(
+        '추천 목록을 받아오는중 에러가 발생하였습니다.',
+        '다시 시도 해주세요.'
+      );
+    }
+    setIsFetching(false);
+  };
+
+  const limitGuideTitle = '사용 횟 수 제한';
+
+  const limitGuideMessage =
+    isAiLimited &&
+    '오늘은 더 이상 사용하실 수 없습니다. 내일 다시 이용해주세요';
+
+  const confirmText = isAiLimited
+    ? `사용제한 (${aiUsageCount}/${MAX_AI_USAGE_COUNT})`
+    : `추천받기 (${aiUsageCount}/${MAX_AI_USAGE_COUNT})`;
+
+  const buttonActions = {
+    core: {
+      title: isAiLimited ? limitGuideTitle : '대주제 추천받기',
+      message:
+        limitGuideMessage ||
+        '작성하신 핵심 목표와 관련된 대주제들을 추천해 드릴게요',
+      confirmText,
+      resolve: handleSuggest,
+    },
+
+    topic: {
+      title: isAiLimited ? limitGuideTitle : '소주제 추천받기',
+      message:
+        limitGuideMessage ||
+        '작성하신 대주제와 관련된 소주제들을 추천해 드릴게요',
+      confirmText,
+      resolve: handleSuggest,
+    },
+  };
+
+  const isDisableSuggestButton = isAiFetching || aiUsageCount === null;
+
   return (
     <div>
       <Button
         size={null}
+        disabled={isAiFetching}
         variant='outline'
-        onClick={
-          // 누르면 모달이나 알럿창떠서 거기서 ai추천 선택할 수 있게 구현
-          type === 'core' ? handleAiTopicSuggest : handleAiSubTopicSuggest
-        }
+        onClick={() => {
+          openAlert(
+            'confirm',
+            buttonActions[type].title,
+            buttonActions[type].message,
+            buttonActions[type].resolve,
+            buttonActions[type].confirmText
+          );
+        }}
       >
         <div className='flex h-10 items-center gap-2 p-3'>
-          <Image
-            alt='ai추천'
-            src='/images/button-image/ai.svg'
-            width={30}
-            height={30}
-          />
-          막막하다면? AI가 도와드릴게요!
+          {isDisableSuggestButton ? (
+            <>
+              <LoaderCircle className='animate-spin' />
+              {aiUsageCount === null
+                ? '사용 횟 수를 불러오는 중입니다..'
+                : 'AI에게 데이터 요청중입니다.'}
+            </>
+          ) : (
+            <>
+              <Image
+                alt='ai추천'
+                src='/images/button-image/ai.svg'
+                width={30}
+                height={30}
+              />
+              막막하다면? AI가 도와드릴게요!
+            </>
+          )}
         </div>
       </Button>
     </div>
--- src/shared/hooks/use-alert-store.ts
+++ src/shared/hooks/use-alert-store.ts
@@ -8,11 +8,13 @@ type AlertState = {
   title: string;
   message: string;
   isLoading: boolean;
+  confirmText: string;
   openAlert: (
     type: AlertType,
     title: string,
     message?: string,
-    promiseResult?: (value: boolean) => void
+    promiseResult?: (value: boolean) => void,
+    confirmText?: string
   ) => void;
   closeAlert: () => void;
   promiseResolve?: (value: boolean) => void;
@@ -33,10 +35,11 @@ export const useAlertStore = create<AlertState>((set) => ({
   type: 'info',
   title: '',
   message: '',
+  confirmText: '확인',
   promiseResolve: undefined,
   isLoading: false,
 
-  openAlert: (type, title, message = '', promiseResult) =>
+  openAlert: (type, title, message = '', promiseResult, confirmText = '확인') =>
     set({
       isOpen: true,
       type,
@@ -44,10 +47,16 @@ export const useAlertStore = create<AlertState>((set) => ({
       message,
       isLoading: false,
       promiseResolve: promiseResult,
+      confirmText,
     }),
 
   closeAlert: () =>
-    set({ isOpen: false, promiseResolve: undefined, isLoading: false }),
+    set({
+      isOpen: false,
+      promiseResolve: undefined,
+      isLoading: false,
+      confirmText: '확인',
+    }),
 
   loadingStart: () => set({ isLoading: true }),
 
