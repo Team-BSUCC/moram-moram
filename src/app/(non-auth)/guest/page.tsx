f4d32fc6fe03cf3fc7880c2c335b673d81a92447
KGyeong <rlarudals61@naver.com>
2025-04-29 03:37:29 +0900

    feature:#224 캔버스 기능 구현 완료

--- src/app/(non-auth)/guest/page.tsx
+++ src/app/(non-auth)/guest/page.tsx
@@ -6,84 +6,63 @@ import Text from '@/components/commons/text';
 import Title from '@/components/commons/title';
 import GuestMandalartBlock from '@/modules/guest/components/guest-mandalart-block';
 import GuestMandalartMainBlock from '@/modules/guest/components/guest-mandalart-main-block';
-import { useGuestTopicStore } from '@/modules/guest/hooks/use-guest-topic-store';
-import { toPng } from 'html-to-image';
-import React, { useEffect, useRef, useState } from 'react';
-import * as Sentry from '@sentry/nextjs';
-import { useOnBeforeUnload } from '@/shared/hooks/use-on-before-unload';
-import { errorAlert } from '@/shared/utils/sweet-alert';
 import GuestModal from '@/modules/guest/components/guest-modal';
 import GuestNavigation from '@/modules/guest/components/guest-navigation';
+import { useGuestTopicStore } from '@/modules/guest/hooks/use-guest-topic-store';
+import {
+  useDownloadMandalartInCanvas,
+  useDownloadMandalartWithOutCanvas,
+} from '@/modules/guest/hooks/use-download-mandalart';
+import { useOnBeforeUnload } from '@/shared/hooks/use-on-before-unload';
+import { usePanzoomController } from '@/modules/guest/hooks/use-canvas-controller';
+import React, { useEffect, useState } from 'react';
 
-const ONE_SECOND = 1000; // 1초
-
-const DESKTOP_SIZE = 1024; // 데스크탑 사이즈
+const DESKTOP_SIZE = 1024;
 
 const GuestPage = () => {
-  const [isDownloading, setIsDownloading] = useState<boolean>(false);
-
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const [isSheetOpen, setIsSheetOpen] = useState<boolean>(false);
-
-  const [isNavigationOpen, setIsNavigationOpen] = useState<boolean>(false);
+  const [isDownloading, setIsDownloading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isSheetOpen, setIsSheetOpen] = useState(false);
+  const [isNavigationOpen, setIsNavigationOpen] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(false);
 
   useEffect(() => {
-    const handleResize = () => {
+    const screenResize = () => {
       if (window.innerWidth >= DESKTOP_SIZE) {
-        return setIsModalOpen(false);
+        setIsDesktop(true);
       }
+
       if (window.innerWidth < DESKTOP_SIZE) {
-        setIsNavigationOpen(false);
-        return setIsSheetOpen(false);
+        setIsDesktop(false);
       }
     };
 
     if (window.innerWidth >= DESKTOP_SIZE) {
-      setIsModalOpen(true);
+      setIsDesktop(true);
     }
 
-    if (window.innerWidth < DESKTOP_SIZE) {
-      setIsSheetOpen(true);
-      setIsNavigationOpen(true);
-    }
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener('resize', screenResize);
+    return () => window.removeEventListener('resize', screenResize);
   }, []);
+
+  const { gridRef, panzoomRef, resetCanvas } = usePanzoomController();
 
   const title = useGuestTopicStore((state) => state.core);
 
-  const ref = useRef<HTMLDivElement>(null);
-
   useOnBeforeUnload(title !== '');
 
-  const handleDownload = async () => {
-    if (ref.current === null) return;
-    setIsDownloading(true);
-    try {
-      const dataUrl = await toPng(ref.current);
-      const link = document.createElement('a');
-      link.download = `${title}.png`;
-      link.href = dataUrl;
-      link.click();
-      setTimeout(() => setIsDownloading(false), ONE_SECOND);
-    } catch (error) {
-      Sentry.withScope((scope) => {
-        scope.setTag('page', 'guest');
-        scope.setTag('feature', 'Img Download Error');
-
-        Sentry.captureException(new Error(`[Guest Page] ${error}`));
-      });
-      errorAlert('이미지 저장에 실패했습니다. 잠시 후 다시 시도해주세요.');
-    }
-  };
+  const { handleDownloadCanvas } = useDownloadMandalartInCanvas(
+    gridRef,
+    panzoomRef,
+    title
+  );
+  const { handleDownload } = useDownloadMandalartWithOutCanvas(gridRef, title);
 
   return (
     <div className='flex flex-col place-items-center'>
       <Spacer size='top' />
-      <div className='bg-white-light p-2 md:p-5'>
-        {/* 만다라트 타이틀 */}
-        <div className='w-full flex-col justify-items-center'>
+      <div className='bg-white-light'>
+        <div className='flex w-full flex-col items-center justify-center justify-items-center'>
           <Title as='h1' size='32px-semibold'>
             나만의 만다라트 작성하기
           </Title>
@@ -98,19 +77,47 @@ const GuestPage = () => {
         </div>
 
         {/* 만다라트 그리드 */}
-        <div
-          ref={ref}
-          className='grid w-[888px] grid-cols-3 grid-rows-3 gap-5 bg-white-light p-3 text-md'
-        >
-          <div className='col-start-2 row-start-2 aspect-square h-full'>
-            <GuestMandalartMainBlock coreColor='bg-violet-pigment' />
-          </div>
-          {Array.from({ length: 8 }).map((_, index) => (
-            <div key={`block${index}`}>
-              <GuestMandalartBlock index={index} />
+
+        {isDesktop ? (
+          // 데스크탑 그리드
+          <div
+            ref={gridRef}
+            className='grid w-[888px] grid-cols-3 grid-rows-3 gap-5 bg-white-light p-3 text-md'
+          >
+            <div className='col-start-2 row-start-2 aspect-square h-full'>
+              <GuestMandalartMainBlock coreColor='bg-violet-pigment' />
             </div>
-          ))}
-        </div>
+            {Array.from({ length: 8 }).map((_, index) => (
+              <div key={`block${index}`}>
+                <GuestMandalartBlock index={index} />
+              </div>
+            ))}
+          </div>
+        ) : (
+          // 모바일 그리드
+          <div className='relative flex h-[100dvw] w-screen flex-1 items-center justify-center overflow-hidden bg-transparent'>
+            <div className='absolute bottom-2 right-2 z-20 items-center rounded-full bg-[rgba(0,0,0,0.3)]'>
+              <button className='p-2 text-md text-white' onClick={resetCanvas}>
+                초기화
+              </button>
+            </div>
+            <div className='pointer-events-none absolute inset-0 z-10 overflow-hidden shadow-[inset_0px_0px_20px_0px_rgba(0,0,0,0.3)]' />
+            <div
+              ref={gridRef}
+              className='z-0 grid min-w-[888px] grid-cols-3 grid-rows-3 gap-5 bg-white-light p-3 text-md'
+              style={{ touchAction: 'none' }}
+            >
+              <div className='col-start-2 row-start-2 aspect-square h-full'>
+                <GuestMandalartMainBlock coreColor='bg-violet-pigment' />
+              </div>
+              {Array.from({ length: 8 }).map((_, index) => (
+                <div key={`block${index}`}>
+                  <GuestMandalartBlock index={index} />
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
 
       {/* 버튼 영역 */}
@@ -118,12 +125,10 @@ const GuestPage = () => {
       <div className='flex w-full justify-center gap-[12px] sm:gap-[14px] md:gap-[16px]'>
         <Button
           onClick={() => {
-            if (window.innerWidth >= DESKTOP_SIZE) {
+            if (window.innerWidth >= 1024) {
               return setIsModalOpen(true);
             }
-            if (window.innerWidth < DESKTOP_SIZE) {
-              return setIsSheetOpen(true);
-            }
+            setIsSheetOpen(true);
           }}
           variant='default'
           size='medium'
@@ -134,11 +139,16 @@ const GuestPage = () => {
           disabled={isDownloading}
           variant='secondary'
           size='medium'
-          onClick={handleDownload}
+          onClick={async () => {
+            setIsDownloading(true);
+            if (panzoomRef === null) return handleDownload(setIsDownloading);
+            handleDownloadCanvas(setIsDownloading);
+          }}
         >
           {isDownloading ? '이미지 저장중...' : '이미지 저장하기'}
         </Button>
       </div>
+
       <GuestModal
         isModalOpen={isModalOpen}
         setIsModalOpen={setIsModalOpen}
