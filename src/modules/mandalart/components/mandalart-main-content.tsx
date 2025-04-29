15380102a2cff5729ee670ee43f6d028e90064e4
KGyeong <rlarudals61@naver.com>
2025-04-29 19:11:38 +0900

    feature:#240 리얼타임 캔버스 기능 추가

--- src/modules/mandalart/components/mandalart-main-content.tsx
+++ src/modules/mandalart/components/mandalart-main-content.tsx
@@ -19,17 +19,22 @@ import { useRealtimeBroadCastRoom } from '../hooks/use-realtime-broadcast-room';
 import AvatarStack from './mandalart-avatar-stack';
 import { useClientStateStore } from '../hooks/use-client-state-store';
 import { formatDate } from '@/modules/dashboard/util/format-date';
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import { RealtimeCursors } from './realtime-cursors';
-import { toPng } from 'html-to-image';
 import { useBroadcastStore } from '../hooks/use-broadcast-store';
 import {
   errorAlert,
   infoAlert,
   successAlert,
 } from '@/shared/utils/sweet-alert';
-import * as Sentry from '@sentry/nextjs';
 import useFloatingSheetStore from '@/shared/hooks/use-floating-sheet-store';
+import { usePanzoomController } from '@/shared/hooks/use-canvas-controller';
+import {
+  useDownloadMandalartInCanvas,
+  useDownloadMandalartWithOutCanvas,
+} from '../hooks/use-download-realtime-mandalart';
+
+const DESKTOP_SIZE = 1024;
 
 type MandalartMainContentProps = {
   user: User | null;
@@ -43,23 +48,21 @@ const MandalartMainContent = ({
   const isVisible = useFloatingSheetStore((state) => state.isVisible);
   const hide = useFloatingSheetStore((state) => state.hide);
 
-  const boardRef = useRef<HTMLDivElement>(null);
-
   useRealtimeBroadCastRoom(`broadcast-room ${mandalartId}`);
   useBatchUpdateTrigger();
   const batchUpdateSupabase = useBroadcastStore(
     (state) => state.batchUpdateSupabase
   );
 
-  const downLoadRef = useRef<HTMLDivElement>(null);
-
   const initialize = useClientStateStore((state) => state.initialize);
   const subTopics = useClientStateStore((state) => state.subTopics);
-  const subtopicDoneList = Array.from(subTopics.values()).filter(
-    (subtopicValue) => subtopicValue.isDone
+  const subtopicDoneList = Array.from(subTopics).filter(
+    ([_, subtopicValue]) => subtopicValue.isDone
   );
 
   const { data, isPending, isError } = useRpcMandalartDataQuery(mandalartId);
+
+  const { gridRef, panzoomRef, resetCanvas } = usePanzoomController();
 
   useEffect(() => {
     if (isPending) return;
@@ -70,26 +73,42 @@ const MandalartMainContent = ({
   const username = getCurrentUserName(user);
   const userId = getCurrentUserId(user);
 
-  const handleDownload = async () => {
-    if (downLoadRef.current === null) return;
+  const [isDownloading, setIsDownloading] = useState<boolean>(false);
+  const [isDesktop, setIsDesktop] = useState<boolean>(false);
 
-    try {
-      const dataUrl = await toPng(downLoadRef.current);
-      const link = document.createElement('a');
-      link.download = `${data?.core.title}.png`;
-      link.href = dataUrl;
-      link.click();
-    } catch (error) {
-      Sentry.withScope((scope) => {
-        scope.setTag('page', 'mandalart');
-        scope.setTag('feature', 'Img Download Error');
-        Sentry.captureException(new Error(`[Img Download Error] ${error}`));
-      });
+  useEffect(() => {
+    const screenResize = () => {
+      if (window.innerWidth >= DESKTOP_SIZE) {
+        setIsDesktop(true);
+      }
+
+      if (window.innerWidth < DESKTOP_SIZE) {
+        setIsDesktop(false);
+      }
+    };
+
+    if (window.innerWidth >= DESKTOP_SIZE) {
+      setIsDesktop(true);
     }
-  };
+
+    window.addEventListener('resize', screenResize);
+    return () => window.removeEventListener('resize', screenResize);
+  }, []);
+
+  const { handleDownloadCanvas } = useDownloadMandalartInCanvas(
+    gridRef,
+    panzoomRef,
+    data?.core.title
+  );
+
+  const { handleDownload } = useDownloadMandalartWithOutCanvas(
+    gridRef,
+    data?.core.title
+  );
 
   if (isPending) return <div>Loading...</div>;
   if (isError) return <div>error</div>;
+
   return (
     <div
       className='flex flex-col items-center'
@@ -114,32 +133,72 @@ const MandalartMainContent = ({
             <Text>{`${formatDate(data.core.startDate)} ~ ${formatDate(data.core.endDate)}`}</Text>
           </div>
           <Spacer size='sm' />
-          <div className='flex'>
-            <BicepsFlexed /> {data.core.subTitle || '반드시 완수한다'}
-            <Text>{data.core.subTitle}</Text>
+          <div className='gap- flex'>
+            <BicepsFlexed />
+            <Text>{data.core.subTitle || '반드시 완수한다'}</Text>
           </div>
         </div>
       </div>
 
-      <div className='flex flex-col items-center md:w-[1088px]'>
-        <div ref={downLoadRef} className='bg-white-light px-8'>
-          <Spacer size='lg' />
-          <LinearProgress value={calculatorProgress(subtopicDoneList.length)} />
-          <Spacer size='lg' />
-          <div
-            className='grid w-fit animate-fadeInOnce grid-cols-3 grid-rows-3 gap-2 text-ss md:gap-5 md:text-md'
-            ref={boardRef}
-          >
-            {/* 중앙 블록 */}
-            <MainBlock />
+      <div className='flex flex-col items-center'>
+        {isDesktop ? (
+          <div className='flex flex-col items-center bg-white-light px-8 md:w-[1088px]'>
+            <Spacer size='lg' />
+            <LinearProgress
+              value={calculatorProgress(subtopicDoneList.length)}
+            />
+            <Spacer size='lg' />
+            <div
+              className='grid w-fit animate-fadeInOnce grid-cols-3 grid-rows-3 gap-2 text-ss md:gap-5 md:text-md'
+              ref={gridRef}
+            >
+              {/* 중앙 블록 */}
+              <MainBlock />
 
-            {/* 나머지 블록 */}
-            {data.topics.map((item, idx) => {
-              return <SubBlock key={item.id} topic={item} index={idx} />;
-            })}
+              {/* 나머지 블록 */}
+              {data.topics.map((item, idx) => {
+                return <SubBlock key={item.id} topic={item} index={idx} />;
+              })}
+            </div>
+            <Spacer size='3xl' />
           </div>
-          <Spacer size='3xl' />
-        </div>
+        ) : (
+          <div className='bg-white-light'>
+            <Spacer size='md' />
+            <div className='px-8'>
+              <LinearProgress
+                value={calculatorProgress(subtopicDoneList.length)}
+              />
+            </div>
+            <Spacer size='md' />
+            <div className='relative flex h-[100dvw] w-screen flex-1 items-center justify-center overflow-hidden bg-transparent'>
+              <div className='absolute bottom-3 right-3 z-10 items-center rounded-full bg-[rgba(0,0,0,0.3)]'>
+                <button
+                  className='p-2 text-md text-white'
+                  onClick={resetCanvas}
+                >
+                  초기화
+                </button>
+              </div>
+              <div className='pointer-events-none absolute inset-0 z-10 overflow-hidden shadow-[inset_0px_0px_20px_0px_rgba(0,0,0,0.3)]' />
+              <div
+                className='z-0 grid min-w-[888px] animate-fadeInOnce grid-cols-3 grid-rows-3 gap-5 text-md'
+                ref={gridRef}
+                style={{ touchAction: 'none' }}
+              >
+                {/* 중앙 블록 */}
+                <MainBlock />
+
+                {/* 나머지 블록 */}
+                {data.topics.map((item, idx) => {
+                  return <SubBlock key={item.id} topic={item} index={idx} />;
+                })}
+              </div>
+            </div>
+            <Spacer size='3xl' />
+          </div>
+        )}
+
         <div className='flex gap-8'>
           <Button
             onClick={() => {
@@ -158,20 +217,30 @@ const MandalartMainContent = ({
           >
             저장하기
           </Button>
-          <Button variant='secondary' onClick={handleDownload}>
-            이미지로 저장하기
+          <Button
+            disabled={isDownloading}
+            variant='secondary'
+            onClick={async () => {
+              setIsDownloading(true);
+              if (panzoomRef === null) return handleDownload(setIsDownloading);
+              handleDownloadCanvas(setIsDownloading);
+            }}
+          >
+            {isDownloading ? '이미지 저장중...' : '이미지 저장하기'}
           </Button>
         </div>
         <Spacer size='3xl' />
       </div>
       {/* 플로팅 시트 */}
       {isVisible && <MandalartFloatingSheet />}
-      <RealtimeCursors
-        roomName={`cursor-room ${mandalartId}`}
-        username={username}
-        userId={userId}
-        boardRef={boardRef}
-      />
+      {isDesktop && (
+        <RealtimeCursors
+          roomName={`cursor-room ${mandalartId}`}
+          username={username}
+          userId={userId}
+          boardRef={gridRef}
+        />
+      )}
     </div>
   );
 };
