1c401f860fa4e2886422abd0bea7a454ea77dbe3
LSJ0706 <zosksldhs@gmail.com>
2025-04-29 17:17:52 +0900

    refactor:#237 불필요한 로그 제거

--- src/shared/utils/delete-auth-cookie.ts
+++ src/shared/utils/delete-auth-cookie.ts
@@ -5,8 +5,6 @@ import { cookies } from 'next/headers';
 export const deleteAuthCookies = async () => {
   const allCookies = cookies().getAll();
 
-  console.log('hi');
-
   allCookies
     .filter((c) => c.name.startsWith('sb-') && c.name.includes('-auth-token'))
     .forEach((c) => {
